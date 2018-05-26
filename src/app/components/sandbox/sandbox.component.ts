0a168abbba384e3407448a54724fdde284275863
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-05-26 17:50:59 +0200

    Style binding & ngStyle

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -3,36 +3,27 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'sandbox',
     template: `
-        <h2 [class.special]="isSpecial">Hello World</h2>
-        <h2 [ngClass]="currentClasses">This text is initially special and saveable</h2>
-    `,
-    styles: [`
-        .special{
-            color: green;
-        }
-
-        .saveable {
-            text-transform: uppercase
-        }
-    `]
+        <h2>Hello World</h2>
+        <div [style.font-size]="isSpecial ? 'x-large': 'smaller'">Font size depending on isSpecial</div>
+        <div [ngStyle]="currentStyles">This div is initially special & saveable</div>
+    `
 })
 
 export class SandboxComponent {
 
     isSpecial: boolean = true;
     canSave: boolean = true;
-    isSaveable: boolean = true;
-    currentClasses = {};
+    currentStyles = {};
 
     constructor() {
         console.log('constructor ran...');
-        this.setCurrentClasses();
+        this.setCurrentStyles();
     }
 
-    setCurrentClasses() {
-        this.currentClasses = {
-            special: this.canSave,
-            saveable: this.isSaveable
+    setCurrentStyles() {
+        this.currentStyles = {
+            'font-style': this.canSave ? 'italic' : 'bold',
+            'font-size': this.isSpecial ? '24px' : '12px'
         }
     }
 }
