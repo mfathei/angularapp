5682e903b938631514496266ca38f2683d99ee13
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-05-26 16:23:57 +0200

    Tempate files and stylesheets

---
+++ src/app/components/sandbox/sandbox.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sandbox',
+    templateUrl: './sandbox.component.html',
+    styleUrls: ['./sandbox.component.css']
+})
+
+export class SandboxComponent {
+
+    constructor() {
+        console.log('constructor ran...');
+    }
+}
