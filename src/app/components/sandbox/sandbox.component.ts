2edeebf9bb5431f550446aff2095870931a1c4ac
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-05-26 17:08:14 +0200

    ngIf & ngSwitch

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -3,33 +3,27 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'sandbox',
     template: `
-        <h4>Hello World</h4>
-        {{ people }}
-        <ul>
-            <li *ngFor="let person of people; let index = index">{{ index }}- {{ person }}</li>
-        </ul>
-        <ul>
-            <li *ngFor="let person of people2">{{ person.firstName }} {{ person.lastName }}</li>
-        </ul>
+        <!-- <h4>Hello 
+        <span *ngIf="showName">{{ name }}</span>
+        <span *ngIf="!showName">World</span>
+        </h4> -->
+        <span *ngIf="showName; else noName">Hello {{ name }}</span>
+        <ng-template #noName> Hello World</ng-template>
+        <hr/>
+        <div [ngSwitch]="greeting">
+            <div *ngSwitchCase="'1'">One</div>
+            <div *ngSwitchCase="'2'">Two</div>
+            <div *ngSwitchCase="'3'">Three</div>
+            <div *ngSwitchDefault>Hello</div>
+        </div>
     `
 })
 
 export class SandboxComponent {
 
-    people = ['Carl', 'Rick', 'Daryl', 'Glen'];
-    people2 = [{
-        firstName: 'Carl',
-        lastName: 'Grimes'
-    },
-    {
-        firstName: 'Rick',
-        lastName: 'Rhee'
-    },
-    {
-        firstName: 'Daryl',
-        lastName: 'Smith'
-    }
-    ];
+    name: string = 'John Doe';
+    showName: boolean = false;
+    greeting: number = 11;
 
     constructor() {
         console.log('constructor ran...');
