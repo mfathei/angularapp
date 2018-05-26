0f53ce4c98734b2d2d707839128323f543ffe083
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-05-26 16:51:59 +0200

    Loop ngFor

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -2,11 +2,34 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'sandbox',
-    templateUrl: './sandbox.component.html',
-    styleUrls: ['./sandbox.component.css']
+    template: `
+        <h4>Hello World</h4>
+        {{ people }}
+        <ul>
+            <li *ngFor="let person of people; let index = index">{{ index }}- {{ person }}</li>
+        </ul>
+        <ul>
+            <li *ngFor="let person of people2">{{ person.firstName }} {{ person.lastName }}</li>
+        </ul>
+    `
 })
 
 export class SandboxComponent {
+
+    people = ['Carl', 'Rick', 'Daryl', 'Glen'];
+    people2 = [{
+        firstName: 'Carl',
+        lastName: 'Grimes'
+    },
+    {
+        firstName: 'Rick',
+        lastName: 'Rhee'
+    },
+    {
+        firstName: 'Daryl',
+        lastName: 'Smith'
+    }
+    ];
 
     constructor() {
         console.log('constructor ran...');
