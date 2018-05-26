550591a3e61f48e2ff9b549f4e62f49e3e288436
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-05-26 18:06:34 +0200

    Pipes

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -3,27 +3,25 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'sandbox',
     template: `
-        <h2>Hello World</h2>
-        <div [style.font-size]="isSpecial ? 'x-large': 'smaller'">Font size depending on isSpecial</div>
-        <div [ngStyle]="currentStyles">This div is initially special & saveable</div>
+        <h4>I was porn in {{ birthday }}</h4>
+        <h4>I was porn in {{ birthday | date}}</h4>
+        <h4>I was porn in {{ birthday | date | uppercase}}</h4>
+        <h4>I was porn in {{ birthday | date:"dd/MM/yyyy" }}</h4>
+        <h4>I was porn in {{ birthday | date:"yyyy" }}</h4>
+        <p>I love {{ 'cake' | uppercase }}</p>
+        <p>I HATE {{ 'CATS' | lowercase }}</p>
+        <h3>Your total is: {{ total | currency }}</h3>
+        <h3>Your total is: {{ total | currency:"GBP" }}</h3>
+        <h3>Your percent is: {{ fee | percent }}</h3>
     `
 })
 
 export class SandboxComponent {
 
-    isSpecial: boolean = true;
-    canSave: boolean = true;
-    currentStyles = {};
+    birthday: Date = new Date(1981, 1, 15);
+    total: number = 500;
+    fee: number = 0.5;
 
     constructor() {
         console.log('constructor ran...');
-        this.setCurrentStyles();
-    }
-
-    setCurrentStyles() {
-        this.currentStyles = {
-            'font-style': this.canSave ? 'italic' : 'bold',
-            'font-size': this.isSpecial ? '24px' : '12px'
-        }
-    }
 }
