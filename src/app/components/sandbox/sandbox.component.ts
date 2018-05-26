42b5fcc95aabb4a0ff72cd684bdfa7d67bfa8e7c
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-05-26 22:26:50 +0200

    Template Driven Forms validation

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -5,11 +5,43 @@ import { Component } from '@angular/core';
     template: `
         <h4>{{ text }}</h4>
         <div class="container">
-            <form (submit)="onSubmit()">
+            <form novalidate  #f="ngForm" (ngSubmit)="onSubmit(f)">
                 <div class="form-group">
                     <label>Name:</label>
-                    <input type="text" [(ngModel)]="name" class="form-control" name="name">
+                    <input type="text" 
+                    [(ngModel)]="user.name" 
+                    class="form-control" 
+                    name="name"
+                    #userName="ngModel"
+                    minlength="2"
+                    required
+                    >
                 </div>
+                <div *ngIf="userName.errors?.required && userName.touched" class="alert alert-danger">Name is required</div>
+                <div *ngIf="userName.errors?.minlength && userName.touched" class="alert alert-danger">Name is at least 2 characters</div>
+                <div class="form-group">
+                    <label>Email:</label>
+                    <input type="email" 
+                    [(ngModel)]="user.email" 
+                    class="form-control" 
+                    name="email"
+                    #userEmail="ngModel"
+                    required
+                    >
+                </div>
+                <div *ngIf="userEmail.errors?.required && userEmail.touched" class="alert alert-danger">Email is required</div>
+                <div class="form-group">
+                    <label>Phone:</label>
+                    <input type="phone" 
+                    [(ngModel)]="user.phone" 
+                    class="form-control" 
+                    name="phone"
+                    #userPhone="ngModel"
+                    minlength="10"
+                    required
+                    >
+                </div>
+                <div *ngIf="userPhone.errors?.required && userPhone.touched" class="alert alert-danger">Please enter a valid phone</div>
                 <input type="submit" value="submit" class="btn btn-success">
             </form>
             <br>
@@ -22,12 +54,20 @@ import { Component } from '@angular/core';
 
 export class SandboxComponent {
 
-    name: string = '';
-    users: string[] = ['Carl', 'John', 'Steve'];
+    user = {
+        name: '',
+        phone: '',
+        email: ''
+    };
 
-    onSubmit() {
-        this.users.push(this.name);
-        this.name = '';
+
+
+    onSubmit({ value, valid }) {
+        if (valid) {
+            console.log(value);
+        } else {
+            console.log('Form is invalid');
+        }
     }
 
 }
