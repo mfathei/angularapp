3a290f46f9f1b4933c744bfb404aaf139af44fdc
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-05-26 15:07:59 +0200

    Adding types to properties

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -3,33 +3,24 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'sandbox',
     template: `
-    <h2>{{ name }} is {{ age }} years old</h2>
-    <h2>{{ person.firstName }} {{ person.lastName }}</h2>
-    <ul>
-        <li>{{ 'Hello World' }}</li>
-        <li>{{ 1 + 1 }}</li>
-        <li>{{ showAge() }}</li>
-    </ul>
+    <h2> Hello World</h2>
     `
 })
 
 export class SandboxComponent {
-    name = "John Doe";
-    age = 35;
-    person = {firstName: "Steve", lastName: "Smith"};
+    name: string = "John Doe";
+    age: number = 35;
+    hasChildren: boolean = true;
+    city: any = 'Boston';
+    myNumbersArray: number[] = [1, 2, 3];
+    myStringArray: string[] = ['hello', 'world'];
+    myAnyArray: any[] = ['Hello', 21];
+    myTuple: [number, string] = [15, 'Mahdy'];
+    unusable: void = undefined;
+    u: undefined = undefined;
+    n: null = null;
 
-    constructor(){
+    constructor() {
         console.log('constructor ran...');
-        // this.age = 36;
-        this.hasBirthday();
-        this.hasBirthday();
-    }
-
-    hasBirthday(){
-        this.age += 1;
-    }
-
-    showAge(){
-        return this.age;
     }
 }
