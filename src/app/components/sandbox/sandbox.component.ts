288c8593b51f28d3957d5e7aff404b60e5c6ebbe
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-05-26 20:17:48 +0200

    Mouse Events

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -4,25 +4,20 @@ import { Component } from '@angular/core';
     selector: 'sandbox',
     template: `
         <h4>I was porn in {{ birthday }}</h4>
-        <h4>I was porn in {{ birthday | date}}</h4>
-        <h4>I was porn in {{ birthday | date | uppercase}}</h4>
-        <h4>I was porn in {{ birthday | date:"dd/MM/yyyy" }}</h4>
-        <h4>I was porn in {{ birthday | date:"yyyy" }}</h4>
-        <p>I love {{ 'cake' | uppercase }}</p>
-        <p>I HATE {{ 'CATS' | lowercase }}</p>
-        <h3>Your total is: {{ total | currency }}</h3>
-        <h3>Your total is: {{ total | currency:"GBP" }}</h3>
-        <h3>Your percent is: {{ fee | percent }}</h3>
+        <button (click)="fireEvent($event)">Click Me</button><br>
+        <button (mouseover)="fireEvent($event)">hover me</button><br>
+        <button (mousedown)="fireEvent($event)">Mouse Down</button><br>
+        <button (mouseup)="fireEvent($event)">Mouse Up</button><br>
+        <button (dblclick)="fireEvent($event)">Double Click Me</button><br>
+        <button (drag)="fireEvent($event)">Drag Me</button><br>
+        <button (dragover)="fireEvent($event)">Drag Me</button><br>
     `
 })
 
 export class SandboxComponent {
 
-    birthday: Date = new Date(1981, 1, 15);
-    total: number = 500;
-    fee: number = 0.5;
-
-    constructor() {
-        console.log('constructor ran...');
+    fireEvent(e){
+        console.log(e.type);
+        
     }
 }
