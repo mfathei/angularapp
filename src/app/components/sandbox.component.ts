7195700ca626c282331286ea10ae3be43ad10904
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-05-26 14:52:32 +0200

    Data binding and string interpolation

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -2,9 +2,34 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'sandbox',
-    template: `<h2>Hello World!</h2>`
+    template: `
+    <h2>{{ name }} is {{ age }} years old</h2>
+    <h2>{{ person.firstName }} {{ person.lastName }}</h2>
+    <ul>
+        <li>{{ 'Hello World' }}</li>
+        <li>{{ 1 + 1 }}</li>
+        <li>{{ showAge() }}</li>
+    </ul>
+    `
 })
 
 export class SandboxComponent {
-    
+    name = "John Doe";
+    age = 35;
+    person = {firstName: "Steve", lastName: "Smith"};
+
+    constructor(){
+        console.log('constructor ran...');
+        // this.age = 36;
+        this.hasBirthday();
+        this.hasBirthday();
+    }
+
+    hasBirthday(){
+        this.age += 1;
+    }
+
+    showAge(){
+        return this.age;
+    }
 }
