64d388da377f2c342b304f1047b28c384eb8411d
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-05-26 17:24:38 +0200

    Property binding

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -3,27 +3,23 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'sandbox',
     template: `
-        <!-- <h4>Hello 
-        <span *ngIf="showName">{{ name }}</span>
-        <span *ngIf="!showName">World</span>
-        </h4> -->
-        <span *ngIf="showName; else noName">Hello {{ name }}</span>
-        <ng-template #noName> Hello World</ng-template>
-        <hr/>
-        <div [ngSwitch]="greeting">
-            <div *ngSwitchCase="'1'">One</div>
-            <div *ngSwitchCase="'2'">Two</div>
-            <div *ngSwitchCase="'3'">Three</div>
-            <div *ngSwitchDefault>Hello</div>
-        </div>
+        <h2>3 ways to bind properties</h2>
+        <div><img src="{{ imageUrl }}"></div>
+        <div><img [src]="imageUrl"></div>
+        <div><img bind-src="imageUrl"></div>
+
+        <div>Image Location: <span [textContent]="imageUrl"></span></div>
+        <hr>
+        <h2>Create Post</h2>
+        <p [hidden]="isUnchanged">Post has been changed, please save</p>
+        <button [disabled]="isUnchanged">Save</button>
     `
 })
 
 export class SandboxComponent {
 
-    name: string = 'John Doe';
-    showName: boolean = false;
-    greeting: number = 11;
+    imageUrl: string = 'http://lorempixel.com/400/200';
+    isUnchanged: boolean = false;
 
     constructor() {
         console.log('constructor ran...');
